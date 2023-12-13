07a3f5d18fbb8ea7a856267d7e31529edc9d83c1
jhpark <komasin4@gmail.com>
2023-12-13 18:34:35 +0900

    수정사항 반영 - 유사도 높은 3개 제품 출력 -> 선택 -> 질의어 변경 및 질의 실행

--- getSimProducts.py
+++ getSimProducts.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd
 import numpy as np
-import openai 
+from openai import AzureOpenAI
 from Levenshtein import distance as levenshtein_distance
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -23,96 +23,37 @@ def read_product_list(file_path):
     except Exception as e:
         print(f"파일을 읽는 중 오류가 발생했습니다: {e}")
         return None
-
-def extract_product_name(question):
-    openai.api_key = os.getenv('OPENAI_API_KEY')  # OpenAI API 키 설정
     
+#def extract_product_name(question):
+
+
+def call_openai_chat(type, question):
+    client = AzureOpenAI(
+        api_key = os.getenv("AZURE_OPENAI_KEY"),  
+        api_version = os.getenv("AZURE_OPENAI_API_VERSION"),
+        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT") 
+    )
+    
+    if(type == 'product'):
+        content = "질문에서 제품명을 추출합니다. 특정 요리 이름도 제품명으로 간주합니다. 제품명만 말해줍니다."
+    else:
+        content = "당신은 대한민국 식품회사 풀무원의 제품 담당자 입니다. 사용자의 제품문의에 대한 답을 합니다."
+
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             # model="gpt-3.5-turbo",
             model="gpt-4",
             messages=[
-                {"role": "system", "content": "질문에서 제품명을 추출합니다."},
+                #{"role": "system", "content": "질문에서 제품명을 추출합니다."},
+                {"role": "system", "content": content},
                 {"role": "user", "content": question}
             ]
         )
-        product_name = response.choices[0].message['content'].strip()
+        product_name = response.choices[0].message.content.strip()
         return product_name
     except Exception as e:
         print(f"OpenAI GPT-3 요청 중 오류가 발생했습니다: {e}")
         return None
-
-def find_similar_products(user_query, product_data):
-    # Levenshtein 거리를 계산하여 가장 가까운 제품을 찾음
-    product_data['Similarity'] = product_data['제품명'].apply(
-        lambda x: levenshtein_distance(user_query, x))
-    sorted_products = product_data.sort_values('Similarity')
-    
-    # 가장 유사한 제품 찾기
-    #best_match = sorted_products.iloc[0]
-    #if best_match['Similarity'] == 0:
-    #    return f"일치하는 제품: NO {best_match['NO']}, 제품명: {best_match['제품명']}"
-    #else:
-    #    # 일치하는 제품이 없는 경우, 가장 유사한 상위 3개의 제품 반환
-    #    top_3_matches = sorted_products.head(3)
-    #    return top_3_matches[['NO', '제품명', 'Similarity']].to_string(index=False)
-    #top_3_matches = sorted_products.head(3)
-    #return top_3_matches[['구분', '대분류', 'NO', '제품명', 'Similarity']].to_string(index=False)
-    return sorted_products.head(3).to_string(index=False)
-    
-def find_similar_products_hamming(user_query, product_data):
-    # Hamming Distance를 이용하여 가장 유사한 제품 찾기
-    # Hamming Distance는 두 문자열의 길이가 동일해야 하므로, 길이를 맞춤
-    max_length = max(len(user_query), max(len(prod)
-                     for prod in product_data['제품명']))
-    user_query_padded = user_query.ljust(max_length)
-
-    product_data['Similarity'] = product_data['제품명'].apply(
-        lambda x: sum(ch1 != ch2 for ch1, ch2 in zip(
-            x.ljust(max_length), user_query_padded)) / max_length
-    )
-
-    # 유사도(낮은 값이 더 유사함)에 따라 제품들 정렬
-    sorted_products = product_data.sort_values('Similarity')
-
-    return sorted_products.head(3).to_string(index=False)
-    #return sorted_products[['구분', '대분류', 'NO', '제품명', 'Similarity']].head(3).to_string(index=False)
-
-def find_similar_products_fuzzywuzzy(user_query, product_data):
-    # FuzzyWuzzy를 이용하여 가장 유사한 제품 찾기
-    product_data['Similarity'] = product_data['제품명'].apply(
-        lambda x: fuzz.ratio(user_query, x)
-    )
-
-    # 유사도(높은 값이 더 유사함)에 따라 제품들 정렬
-    sorted_products = product_data.sort_values('Similarity', ascending=False)
-
-    #return sorted_products[['구분', '대분류', 'NO', '제품명', 'Similarity']].head(3).to_string(index=False)
-    return sorted_products.head(3).to_string(index=False)
-    
-
-def ngram_similarity(text1, text2, n=2):
-    """
-    두 문자열 간의 n-gram 기반 코사인 유사도를 계산합니다.
-    """
-    vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
-    ngrams = vectorizer.fit_transform([text1, text2])
-    return cosine_similarity(ngrams)[0, 1]
-
-def find_similar_products_ngram(user_query, product_data, n=2):
-    """
-    n-gram 유사도를 이용하여 가장 유사한 제품을 찾습니다.
-    """
-    product_data['Similarity'] = product_data['제품명'].apply(
-        lambda x: ngram_similarity(user_query, x, n=n)
-    )
-
-    # 유사도(높은 값이 더 유사함)에 따라 제품들 정렬
-    sorted_products = product_data.sort_values('Similarity', ascending=False)
-
-    #return sorted_products[['구분', '대분류', 'NO', '제품명', 'Similarity']].head(3).to_string(index=False)
-    return sorted_products.head(3).to_string(index=False)
-
 
 def find_similar_products_sequence_matching(user_query, product_data):
     """
@@ -132,72 +73,110 @@ def find_similar_products_sequence_matching(user_query, product_data):
         by='Similarity', ascending=False)
 
     # 상위 3개 제품 반환
-    return sorted_products.head(3).to_string(index=False)
+    return sorted_products.head(3)
 
-# 예시 파일 경로와 사용자 질문
-#file_path = "/Users/komasin4/Data/maum.ai/pulmuwon/prodlist_2.csv"
-file_path = "/Users/komasin4/Data/maum.ai/pulmuwon/mergelist_1.csv"
-# 제품 목록 읽기
-product_data = read_product_list(file_path)
+def printPrds():
+    products = ""
+    for index, row in product_data.iterrows():
+        products += row.to_string(index=False).replace(" ",
+                                                    "").replace("\n", ".") + "\n"
 
+    st.text_area('참고 - 제품목록', products, height=500)    
 
-#화면
-st.title('유사제품 추출 테스트')
 
 if 'prev_question' not in st.session_state:
     st.session_state.prev_question = ''
 if 'product_name' not in st.session_state:
     st.session_state.product_name = ''
+if 'action_step' not in st.session_state:
+    st.session_state.action_step = '0'
+if 'similar_products' not in st.session_state:
+    st.session_state.similar_products = []
+if 'selected_item' not in st.session_state:
+    st.session_state.selected_item = ''
+if 'item_list' not in st.session_state:
+    st.session_state.item_list = []
+if 'desc_list' not in st.session_state:
+    st.session_state.desc_list = []
+
+print("1.st.session_state.action_step", st.session_state.action_step)
+
+# 제품 목록 읽기
+file_path = "/Users/komasin4/Data/maum.ai/pulmuwon/mergelist_1.csv"
+product_data = read_product_list(file_path)
+
+#화면
+st.title('유사제품 추출 테스트')
 
 # question = st.text_input('질문', placeholder='질문을 입력하세요.')
 question = st.text_input('질문내용입력', '제주 보리차는 얼마인가요?')
 
-al = st.radio(
-    "알고리즘 선택",
-    ["Levenshtein Distance", "Hamming Distance", "FuzzyWuzzy", "N-gram Analysis", "Sequence Matching"])
-    #captions=["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+#al = st.radio(
+#    "알고리즘 선택",
+#    ["Levenshtein Distance", "Hamming Distance", "FuzzyWuzzy", "N-gram Analysis", "Sequence Matching"])
+#    #captions=["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
 
 if st.button('물어보기!!!'):
+    print("if-1")
+    
+    print("1|", question, "|", st.session_state.prev_question)
+    
     with st.spinner('기다려주세요...'):
-        print("1.prev_question", st.session_state.prev_question)
-        print("2.question", question)
-
         if st.session_state.prev_question != question:
-            print("제품명 변경!!!")
-            st.session_state.product_name = extract_product_name(question)
-            logger.info("execute go!!!:" + question)
-               
-        #product_name = "test"
+            print("extract product name...")
+            st.session_state.product_name = call_openai_chat('product', question)
+            st.session_state.prev_question = question
         st.write("제품명 : ", st.session_state.product_name)
+        st.session_state.similar_products = find_similar_products_sequence_matching(
+            st.session_state.product_name, product_data)
         
-        if al == 'Levenshtein Distance':
-            similar_products = find_similar_products(
-                st.session_state.product_name, product_data)
-        elif al == 'Hamming Distance':
-            similar_products = find_similar_products_hamming(
-                st.session_state.product_name, product_data)
-        elif al == 'FuzzyWuzzy':
-            similar_products = find_similar_products_fuzzywuzzy(
-                st.session_state.product_name, product_data)
-        elif al == 'N-gram Analysis':
-            similar_products = find_similar_products_ngram(
-                st.session_state.product_name, product_data)
-        elif al == 'Sequence Matching':
-            similar_products = find_similar_products_sequence_matching(
-                st.session_state.product_name, product_data)
+        st.session_state.item_list = list()
+        st.session_state.desc_list = list()
 
-        st.text_area(al, similar_products)
+        for index, row in st.session_state.similar_products.iterrows():
+            st.session_state.item_list.append(row.제품명)
+            st.session_state.desc_list.append("순번:" + str(row.NO) +
+                           ", 유사도:" + str(row.Similarity))
+
+        st.session_state.selected_item = st.radio(
+            "제품을 선택하세요", st.session_state.item_list, captions=st.session_state.desc_list, index=None)
         
-        st.session_state.prev_question = question
+        print("st.session_state.selected_item", st.session_state.selected_item)
+
+        print("2|", question, "|", st.session_state.prev_question)
         
-        print("2.prev_question", st.session_state.prev_question)
-        print("2.question", question)
+        st.session_state.action_step = 1
+elif st.session_state.action_step == 1:
+    print("if-2")
+    #if st.session_state.prev_question != question:
+    #    st.session_state.product_name = extract_product_name(question)
+    st.write("제품명 : ", st.session_state.product_name)
+    st.session_state.similar_products = find_similar_products_sequence_matching(
+        st.session_state.product_name, product_data)
+    #st.session_state.prev_question = question
+        
+    st.session_state.selected_item = st.radio(
+        "제품을 선택하세요", st.session_state.item_list, captions=st.session_state.desc_list, index=None)
+
+    print("st.session_state.selected_item", st.session_state.selected_item)
+    print(st.session_state.product_name, "->", st.session_state.selected_item)
+
+    new_question = question.replace(
+        st.session_state.product_name, st.session_state.selected_item)
+
+    st.write("수정질문: ", new_question)
+    
+    with st.spinner('제품에 대해 문의 중입니다. 잠시만 기다려주세요...'):
+        answer = call_openai_chat('question', new_question)
+        st.text_area("답변", answer)
+
+    print("3|", question, "|", st.session_state.prev_question)
+
+    
+
+printPrds()
+print("2.st.session_state.action_step", st.session_state.action_step)
 
 
-products = ""
 
-for index, row in product_data.iterrows():
-    products += row.to_string(index=False).replace(" ",
-                                                   "").replace("\n", ".") + "\n"
 
-st.text_area('제품목록', products, height=500)
